5971cdb7867650e049153fe1ce67fb698c65405f
Chirayu Desai <chirayudesai1@gmail.com>
2021-11-12 04:48:12 +0530

    device-calyx: Set TARGET_PREBUILT_KERNEL
    Change-Id: I156a4af77bd619f855f734983b24677531156c4d

--- device-calyx.mk
+++ device-calyx.mk
@@ -8,3 +8,5 @@ PRODUCT_COPY_FILES += \
 # For Google Camera
 PRODUCT_COPY_FILES += \
     device/google/raviole/the_experiences.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/the_experiences.xml
+
+TARGET_PREBUILT_KERNEL := device/google/raviole-kernel/Image.lz4
