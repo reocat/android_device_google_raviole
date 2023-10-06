b18097295d8bd9818ef27221a229a535c88e7f91
Michael Bestas <mkbestas@gmail.com>
2023-10-06 20:03:59 +0300

    Include wireless_charger makefile
    This includes the required sepolicy and compatibility matrix
    in the build.
    
    Change-Id: I3096357c5f45e664a7b39870fdc053e48d1361ad

--- device-calyx.mk
+++ device-calyx.mk
@@ -11,6 +11,9 @@ PRODUCT_COPY_FILES += \
 
 TARGET_PREBUILT_KERNEL := device/google/raviole-kernel/Image.lz4
 
+# wireless_charger HAL service
+include device/google/gs-common/wireless_charger/wireless_charger.mk
+
 # Build necessary packages for vendor
 
 # Audio
