bfadd2b4678269a89f6510074d6e58e6beedcab7
Michael Bestas <mkbestas@gmail.com>
2023-12-07 23:45:00 +0200

    proprietary-files: UQ1A.231205.015
    Change-Id: Ief21d6ba87256bcbe9e60be30ed05cd5ce5fed5b

--- device-calyx.mk
+++ device-calyx.mk
@@ -1,5 +1,9 @@
 DEVICE_PACKAGE_OVERLAYS += device/google/raviole/overlay-calyx
 
+# ANGLE - Almost Native Graphics Layer Engine
+PRODUCT_PACKAGES += \
+    ANGLE
+
 # EUICC
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.telephony.euicc.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/android.hardware.telephony.euicc.xml \
@@ -30,15 +34,8 @@ PRODUCT_PACKAGES += \
 
 # Bluetooth
 PRODUCT_PACKAGES += \
-    android.hardware.bluetooth@1.0.vendor:64 \
-    android.hardware.bluetooth@1.1.vendor:64 \
-    hardware.google.bluetooth.bt_channel_avoidance@1.0.vendor:64 \
-    hardware.google.bluetooth.ccc@1.0.vendor:64 \
-    hardware.google.bluetooth.ccc@1.1.vendor:64 \
-    hardware.google.bluetooth.ewp@1.0.vendor:64 \
-    hardware.google.bluetooth.ext@1.0.vendor:64 \
-    hardware.google.bluetooth.sar@1.0.vendor:64 \
-    hardware.google.bluetooth.sar@1.1.vendor:64
+    android.hardware.bluetooth-V1-ndk.vendor:64 \
+    hardware.google.bluetooth.bt_channel_avoidance@1.0.vendor:64
 
 # Camera
 PRODUCT_PACKAGES += \
@@ -63,9 +60,9 @@ PRODUCT_PACKAGES += \
 
 # Confirmation UI
 PRODUCT_PACKAGES += \
-    android.hardware.confirmationui@1.0.vendor:64 \
     android.hardware.confirmationui-V1-ndk.vendor:64 \
     android.hardware.confirmationui-lib.trusty:64 \
+    android.hardware.confirmationui@1.0.vendor:64 \
     libteeui_hal_support.vendor:64
 
 # Graphics
@@ -84,6 +81,10 @@ PRODUCT_PACKAGES += \
     android.hardware.identity-V5-ndk.vendor:64 \
     android.hardware.identity-support-lib.vendor:64 \
     android.hardware.identity_credential.xml
+
+# Json
+PRODUCT_PACKAGES += \
+    libjson:64
 
 # Nos
 PRODUCT_PACKAGES += \
@@ -132,6 +133,7 @@ PRODUCT_PACKAGES += \
     android.hardware.authsecret-V1-ndk.vendor:64 \
     android.hardware.biometrics.common-V2-ndk.vendor:64 \
     android.hardware.biometrics.fingerprint-V2-ndk.vendor:64 \
+    android.hardware.gnss-V3-ndk.vendor:64 \
     android.hardware.health-V1-ndk.vendor \
     android.hardware.input.common-V1-ndk.vendor:64 \
     android.hardware.input.processor-V1-ndk.vendor:64 \
@@ -139,6 +141,10 @@ PRODUCT_PACKAGES += \
     android.hardware.keymaster@4.0.vendor:64 \
     android.hardware.keymaster@4.1.vendor:64 \
     android.hardware.neuralnetworks-V4-ndk.vendor:64 \
+    android.hardware.neuralnetworks@1.0.vendor:64 \
+    android.hardware.neuralnetworks@1.1.vendor:64 \
+    android.hardware.neuralnetworks@1.2.vendor:64 \
+    android.hardware.neuralnetworks@1.3.vendor:64 \
     android.hardware.oemlock-V1-ndk.vendor:64 \
     android.hardware.power@1.0.vendor:64 \
     android.hardware.power@1.1.vendor:64 \
@@ -152,10 +158,9 @@ PRODUCT_PACKAGES += \
     android.hardware.radio@1.4.vendor \
     android.hardware.radio@1.5.vendor \
     android.hardware.radio@1.6.vendor \
+    android.hardware.thermal-V1-ndk.vendor:32 \
     android.hardware.thermal@1.0.vendor:32 \
     android.hardware.thermal@2.0.vendor:32 \
-    android.hardware.thermal-V1-ndk.vendor:32 \
     android.hardware.weaver-V2-ndk.vendor:64 \
     android.hardware.wifi-V1-ndk.vendor:64 \
-    com.google.hardware.pixel.display-V4-ndk.vendor:64 \
-    com.google.hardware.pixel.display-V6-ndk.vendor
+    com.google.hardware.pixel.display-V4-ndk.vendor:64
