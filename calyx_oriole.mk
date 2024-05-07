99f4aa3595cf4c8d0bb4d88b589306febfe6fc0b
Chirayu Desai <chirayudesai1@gmail.com>
2024-05-08 01:12:17 +0530

    Update fingerprint/build description from AP1A.240505.004
    Change-Id: I53bde4646fe40bc064a8e8dea8e9b35da754afd2

--- calyx_oriole.mk
+++ calyx_oriole.mk
@@ -14,9 +14,9 @@ PRODUCT_BRAND := google
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     TARGET_PRODUCT=oriole \
-    PRIVATE_BUILD_DESC="oriole-user 14 AP1A.240405.002 11480754 release-keys"
+    PRIVATE_BUILD_DESC="oriole-user 14 AP1A.240505.004 11583682 release-keys"
 
-BUILD_FINGERPRINT := google/oriole/oriole:14/AP1A.240405.002/11480754:user/release-keys
+BUILD_FINGERPRINT := google/oriole/oriole:14/AP1A.240505.004/11583682:user/release-keys
 
 PRODUCT_RESTRICT_VENDOR_FILES := false
 
