eb198efa86499fbe04692a789504c4cae753020e
Chirayu Desai <chirayudesai1@gmail.com>
2024-08-06 18:03:43 -0400

    Update fingerprint/build description from AP2A.240805.005.F1
    Change-Id: I8df4653d576298f9250409d2708e16b34db013d5

--- calyx_raven.mk
+++ calyx_raven.mk
@@ -14,9 +14,9 @@ PRODUCT_BRAND := google
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     TARGET_PRODUCT=raven \
-    PRIVATE_BUILD_DESC="raven-user 14 AP2A.240705.004 11875680 release-keys"
+    PRIVATE_BUILD_DESC="raven-user 14 AP2A.240805.005.F1 12043167 release-keys"
 
-BUILD_FINGERPRINT := google/raven/raven:14/AP2A.240705.004/11875680:user/release-keys
+BUILD_FINGERPRINT := google/raven/raven:14/AP2A.240805.005.F1/12043167:user/release-keys
 
 PRODUCT_RESTRICT_VENDOR_FILES := false
 
